3d737ec5b71bfa973f3b969f57989b5127ac6243
optima211 <nedmk@mail.ru>
2019-02-19 13:41:36 +0700

    debug error -select-

--- admin123/urls.py
+++ admin123/urls.py
@@ -1,10 +1,11 @@
-from . import views
-from django.urls import path, include, re_path
+# from . import views
+from django.urls import path, re_path
 from django.views.generic import ListView, DetailView
-from news.models import Admins
+from admin123.models import Admins
 
 urlpatterns = [
-    path('', ListView.as_view(queryset=Admins.objects.all().order_by("-date")[:20], template_name="news/posts.html")),
+    path('', ListView.as_view(queryset=Admins.objects.all().order_by("-date")[:20],
+                              template_name="admin123/admin-select.html")),
     # path('', views.index, name='index'),
     re_path(r'^(?P<pk>\d+)$', DetailView.as_view(model=Admins, template_name="admin123/admin-select.html")),
 ]
