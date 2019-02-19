ebfe25d0cc1846e25f726309ba169a305e0ac7d1
optima211 <nedmk@mail.ru>
2019-02-19 14:07:18 +0700

    add feature (correct roles)

--- admin123/models.py
+++ admin123/models.py
@@ -11,3 +11,10 @@ class Admins(models.Model):
 
     def __str__(self):
         return self.login
+
+
+class Roles(models.Model):
+    NameRole = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.NameRole
