a0cb8354a89a6ebc743033164080bdf54cfdda82
optima211 <nedmk@mail.ru>
2019-02-21 10:29:02 +0700

    correct conclusion table-data

--- admin123/urls.py
+++ admin123/urls.py
@@ -1,11 +1,12 @@
-# from . import views
+from . import views
 from django.urls import path, re_path
-from django.views.generic import ListView, DetailView
+from django.views.generic import DetailView
 from admin123.models import Admins
 
 urlpatterns = [
-    path('', ListView.as_view(queryset=Admins.objects.all().order_by("-date")[:20],
-                              template_name="admin123/admin-select.html")),
+    # path('', ListView.as_view(queryset=Admins.objects.all().order_by("-date")[:20],
+    #                           template_name="admin123/admin-select.html")),
     # path('', views.index, name='index'),
     re_path(r'^(?P<pk>\d+)$', DetailView.as_view(model=Admins, template_name="admin123/admin-select.html")),
+    path('', views.ShowUsersView.as_view(template_name="admin123/admin-select.html"))
 ]
--- admin123/views.py
+++ admin123/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Admins
 
 
 # Create your views here.
 def index(request):
     return render(request, 'admin123/admin-select.html')
+
+
+class ShowUsersView(ListView):
+    model = Admins
+    template_name = 'admin123/admin-select.html'
+    context_object_name = 'admins'
+    ordering = ['-date']
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        ctx = super(ShowUsersView, self).get_context_data(**kwargs)
+        ctx['title'] = 'Главная страница панели администратора'
+        return ctx
